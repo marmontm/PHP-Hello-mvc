abbf4dc07a85164a7639021e8fc57eaf5b6b2c46
Maxime MARMONT <marmontm@users.noreply.github.com>
2018-01-25 23:58:49 +0200

    Before implementing MVC model

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: maxime
- * Date: 25/01/2018
- * Time: 13:35
- */
 
 namespace App\Controller;
 
--- src/Controller/Model.php
+++ src/Controller/Model.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: maxime
- * Date: 25/01/2018
- * Time: 13:35
- */
 
 namespace App\Controller;
 
 
 class Model
 {
-
+    var $msg = 'bonjour';
 }
